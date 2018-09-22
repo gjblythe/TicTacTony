b79cdeff5382a3efa38ebd19fe7d1cdf1e856ebf
Garrett <gjblythe@gmail.com>
2018-09-22 12:01:41 -0700

    user

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,22 +10,33 @@ firebase.initializeApp(config);
 
 gameDb = firebase.database();
 
-
-var playerOne = "X";
-var playerTwo = "O";
+// var playerOne = "X";
+// var playerTwo = "O";
 var playerOneWins = 0;
 var playerTwoWins = 0;
 var playerOneLoses = 0;
 var playerTwoLoses = 0;
+var win = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+];
 var tie = 0;
 var progress = 9;
+playerOne = "";
+playerTwo = "";
 
 //game firebase start place a place a player into the db
-function recordGame(){
-gameDb.ref().push({
-  playerOne: playerOneTony,
-  playerTwo: playerTwoTony
-});
+function recordGame() {
+  gameDb.ref().push({
+    playerOne: playerOneTony,
+    playerTwo: playerTwoTony
+  });
 }
 
 var arrayOfTonys = [
@@ -41,72 +52,61 @@ var arrayOfTonys = [
   "Fat Tony"
 ];
 
-//use chance to grab an index
-
-//needs a button
-
-//change to random on function
-
-
 playerOneTony = "";
 playerTwoTony = "";
 
 var gameOver = false;
 
-var playerOneSquares = {
-  tLeft: 0,
-  tCenter: 0,
-  tRight: 0,
-  left: 0,
-  center: 0,
-  right: 0,
-  bLeft: 0,
-  bCenter: 0,
-  bRight: 0
-};
+function random() {
+  chance.integer({ min: 1, max: 9 });
+}
 
-var playerTwoSquares = {
-  tLeft: 0,
-  tCenter: 0,
-  tRight: 0,
-  left: 0,
-  center: 0,
-  right: 0,
-  bLeft: 0,
-  bCenter: 0,
-  bRight: 0
-};
-
-
-function random(){
-  chance.integer({min: 1, max: 9});
-};
-//fires when user clicks p1 p2
-//needs a button
-
-//change to random on function
 function tonyRandom() {
   i = chance.integer({ min: 0, max: 9 });
   j = chance.integer({ min: 0, i, max: 9 });
   playerOneTony = arrayOfTonys[i];
   playerTwoTony = arrayOfTonys[j];
-  console.log(playerOneTony, playerTwoTony);
-  
-
 }
+function userSelect() {
+  $("#x").click(function() {
+    playerOne = playerOneTony;
+    playerTwo = playerTwoTony;
+    $("#choice").hide();
+    $("#board").show();
+    playerScore();
+    console.log("p1"+playerOne, "p2"+playerTwo);
+  });
 
+  $("#o").click(function() {
+    playerOne = playerTwoTony;
+    playerTwo = playerOneTony;
+    $("#choice").hide();
+    $("#board").show();
+    playerScore();
+    console.log("p1"+playerOne, "p2"+playerTwo);
+  });
+};
+
+function playerScore(){
+  $('#players').show()
+}
+function gameOver(){
+  $("#new-game").show();
+  $("#new-game").click(function(){
+    $("#new-game").hide();
+    $("#choice").show();
+  })
+}
 //computer choice
-function randomComputer (){
- if (compChoice === selected){
-
- }
-
- }
+function randomComputer() {
+  if (compChoice === selected) {
+  }
+}
 
 //which player starts
 function whoStarts() {
   var coinFlip = chance.coin();
-  console.log(coinFlip)
+  console.log(coinFlip);
   if (coinFlip === "heads") {
     //user
   } else {
@@ -138,45 +138,29 @@ function whoStarts() {
 //});
 //}
 
-$(document).ready(function () {
-  var arrayOfTonys = [
-    "Tony+Soprano",
-    "Tony+The+Tiger",
-    "Tony+Montana",
-    "Tony+Hawk",
-    "Iron+Man",
-    "Tony+Danza",
-    "Tony+Bennett",
-    "Tony+Jaa",
-    "Anthony+Hopkins",
-    "Fat+Tony"
-  ];
-  //change to random on function
-  function tonyRandom() {
-    i = chance.integer({ min: 0, max: 9 });
-    j = chance.integer({ min: 0, i, max: 9 });
-    playerOneTony = arrayOfTonys[i];
-    playerTwoTony = arrayOfTonys[j];
-    console.log(playerOneTony, playerTwoTony);
-  };
 
-  $(document).on('click', 'a', function () {
+function apiCallPlayerOne(){
+  $("button").on("click", "a", function() {
     //value of playerOneTony on button
+   
     tonyRandom();
-    var playerOneTony = $(this).html();
-    console.log(playerOneTony);
+
+    console.log(playerOne);
     //API with key, limit of 10, and rating of G.
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + playerOneTony + "&api_key=NT48m4Vbdp0xJS0bh8cJv7zdIA0X4y8X&limit=10&rating=R";
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      playerOne+
+      "&api_key=NT48m4Vbdp0xJS0bh8cJv7zdIA0X4y8X&limit=10&rating=R";
     console.log(queryURL);
     // AJAX Function to API
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
       var results = response.data;
       console.log(results);
       //jQuery to empty div.
-      $('.btn').empty();
+      $(".btn").empty();
       //For loop for images retrieved
       // for (var j = 0; j < results.length; j++) {
       //   var imageView = results[j].images.fixed_height.url;
@@ -190,19 +174,67 @@ $(document).ready(function () {
     });
     //Function for still and animate .gis retrieved.
     function playGif() {
-      var state = $(this).attr('data-state');
+      var state = $(this).attr("data-state");
       console.log(state);
-      if (state == 'still') {
-        $(this).attr('src', $(this).data('animate'));
-        $(this).attr('data-state', 'animate');
-      }
-      else {
-        $(this).attr('src', $(this).data('still'));
-        $(this).attr('data-state', 'still');
+      if (state == "still") {
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
       }
     }
   });
+};
+
+
+//player two call
+function apiCallPlayerTwo(){
+$("button").on("click", "a", function() {
+  tonyRandom();
+  //value of playerOneTony on button
+  //API with key, limit of 10, and rating of G.
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    playerTwo+
+    "&api_key=NT48m4Vbdp0xJS0bh8cJv7zdIA0X4y8X&limit=10&rating=R";
+  console.log(queryURL);
+  // AJAX Function to API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var results = response.data;
+    console.log(results);
+    //jQuery to empty div.
+    $(".btn").empty();
+    //For loop for images retrieved
+    // for (var j = 0; j < results.length; j++) {
+    //   var imageView = results[j].images.fixed_height.url;
+    //   var still = results[j].images.fixed_height_still.url;
+    //   console.log(imageView);
+    //   var gifImage = $('<img>').attr("src", still).attr('data-animate', imageView).attr('data-still', still);
+    //   gifImage.attr('data-state', 'still');
+    //   $('.btn').append(gifImage);
+    //   gifImage.on('click', playGif);
+    // }
+  });
+  //Function for still and animate .gis retrieved.
+  function playGif() {
+    var state = $(this).attr("data-state");
+    console.log(state);
+    if (state == "still") {
+      $(this).attr("src", $(this).data("animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).data("still"));
+      $(this).attr("data-state", "still");
+    }
+  }
 });
+};
+
+
 // psydocode
 progress--;
 function game() {
@@ -242,15 +274,14 @@ function game() {
   }
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   console.log("ready!");
   tonyRandom();
-
-  playerOneSelect();
-  console.log(playerOneSelect);
-  playerTwoSelect();
-  console.log(playerTwoSelect);
-
   whoStarts();
-
+  userSelect();
+  $('#players').hide();
+  $("#board").hide();
+  $("#new-game").hide();
+  apiCallPlayerOne();
+  apiCallPlayerTwo();
 });
